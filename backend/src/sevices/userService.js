8190594ba043caf8c9fd502cd4626ab225c6ba27
Anton Tutolmin <crysisage@gmail.com>
2020-07-15 02:49:48 +0600

    fix user service

--- backend/src/sevices/userService.js
+++ backend/src/sevices/userService.js
@@ -1,48 +1,51 @@
 const UserResource = require('../resources/user');
 const Bcrypt = require('../config/bcrypt');
 
-const userService = {
-
-  getAllUsers: async () => {
-    const users = await UserResource.getAllUsers();
-    return users;
-  },
-
-  getUserById: async (id) => {
-    const user = UserResource.getUserById(id);
-    return user;
-  },
-
-  getUserByUsername: async (username) => {
-    const user = await UserResource.getUserByUsername(username);
-    return user;
-  },
-
-  createUser: async (body) => {
-    const hashedPassword = await Bcrypt.hashPassword(body.password);
-    const params = {
-      username: body.username,
-      email: body.email,
-      phone: body.phone,
-      password: hashedPassword
-    }
-    const user = await UserResource.createUser(params);
-    return user;
-  },
-
-  updateUserById: async (id, body) => {
-    const params = {
-      username: body.username,
-      email: body.email,
-      phone: body.phone
-    }
-    await UserResource.updateUserById(id, params);
-  },
-
-  deleteUserById: async (id) => {
-    await UserResource.deleteUserById(id);
-  }
-
+async function getAllUsers() {
+  const users = await UserResource.getAllUsers();
+  return users;
 }
 
-module.exports = userService;
+async function getUserById(id) {
+  const user = UserResource.getUserById(id);
+  return user;
+}
+
+async function getUserByUsername(username) {
+  const user = await UserResource.getUserByUsername(username);
+  return user;
+}
+
+async function createUser(body) {
+  const hashedPassword = await Bcrypt.hashPassword(body.password);
+  const params = {
+    username: body.username,
+    email: body.email,
+    phone: body.phone,
+    password: hashedPassword
+  }
+  const user = await UserResource.createUser(params);
+  return user;
+}
+
+async function updateUserById(id, body) {
+  const params = {
+    username: body.username,
+    email: body.email,
+    phone: body.phone
+  }
+  await UserResource.updateUserById(id, params);
+}
+
+async function deleteUserById(id) {
+  await UserResource.deleteUserById(id);
+}
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  getUserByUsername,
+  createUser,
+  updateUserById,
+  deleteUserById
+};
