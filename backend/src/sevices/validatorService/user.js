3f7d79d9039d363df02e71530e6b62a6152ebcca
Anton Tutolmin <crysisage@gmail.com>
2020-07-15 02:37:56 +0600

    fix validator user request

--- backend/src/sevices/validatorService/user.js
+++ backend/src/sevices/validatorService/user.js
@@ -1,50 +1,67 @@
 
-
-
-const UserValidator = {
-
-  validateLoginBody: (body) => {
-
-  },
-
-  validateCreateBody: (body) => {
-
-  },
-
-  validateUpdateBody: (body) => {
-    const keys = Object.keys(body);
-
-    for (let key of keys) {
-
-    }
-  },
-
-  validatePassword: (password) => {
-    if (!password || password.length < 5) {
-      throw new Error('Password is not correct');
-    }
-  },
-
-  validateUsername: (username) => {
-    if (!username || username.length < 5) {
-      throw new Error('Username is not correct')
-    }
-  },
-
-  validatePhone: (phone) => {
-    if (!phone || typeof phone !== 'string' || phone.match(/[^0-9]/g).length !== 0) {
-      throw new Error('Phone is not correct');
-    }
-  },
-
-  validateEmail: (email) => {
-    if (!email || typeof email !== 'string' || phone.match(/\w@[gmail|email|yandex].[com|ru]$/g).length !== 0) {
-      throw new Error('Email is not correct');
-    }
-  },
-
-
-
+function validateLoginBody(body) {
+  validateUsername(body.username);
+  validatePassword(body.password)
 }
 
-module.exports = UserValidator;
+function validateCreateBody(body) {
+  validateUsername(body.username);
+  validatePassword(body.password);
+  validateEmail(body.email);
+  validatePhone(body.phone);
+}
+
+function validateUpdateBody(body) {
+  for (let param of Object.keys(body)) {
+    if (param === 'username') {
+      validateUsername(body[param]);
+    }
+
+    if (param === 'email') {
+      validateEmail(body[param]);
+    }
+
+    if (param === 'phone') {
+      validatePhone(body[param]);
+    }
+  }
+}
+
+function validatePassword(password) {
+  if (!password || password.length < 5) {
+    throw new Error('Password is not correct');
+  }
+}
+
+function validateUsername(username) {
+  if (!username || username.length < 5) {
+    throw new Error('Username is not correct')
+  }
+}
+
+function validatePhone(phone) {
+  if (
+    !phone ||
+    typeof phone !== 'string' ||
+    phone.length !== 11 ||
+    phone.match(/[^0-9]/g).length !== 0
+  ) {
+    throw new Error('Phone is not correct');
+  }
+}
+
+function validateEmail(email) {
+  if (
+    !email ||
+    typeof email !== 'string' ||
+    phone.match(/\w@[gmail|email|yandex].[com|ru]$/g).length !== 0
+  ) {
+    throw new Error('Email is not correct');
+  }
+}
+
+module.exports = {
+  validateLoginBody,
+  validateCreateBody,
+  validateUpdateBody
+};
