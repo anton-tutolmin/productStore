54b51b1d0e97a9ab1e53990a248403a230295257
Anton Tutolmin <crysisage@gmail.com>
2020-07-15 02:53:36 +0600

    fix user resource

--- backend/src/resources/user.js
+++ backend/src/resources/user.js
@@ -1,43 +1,47 @@
 const User = require('../models/User');
 
-const UserResource = {
-
-  getAllUsers: async () => {
-    const users = await User.find({});
-    return users;
-  },
-
-  getUserById: async (id) => {
-    const user = await User.findOne({_id: id});
-    return user;
-  },
-
-  getUserByUsername: async (username) => {
-    const user = await User.findOne({username});
-    return user;
-  },
-
-  createUser: async (params) => {
-    const user = await User.create({
-      ...params
-    });
-    return user;
-  },
-
-  updateUserById: async (id, params) => {
-    await User.updateOne(
-      {
-        _id: id
-      },
-      {
-        ...params
-      }
-    );
-  },
-
-  deleteUserById: async (id) => {
-    await User.deleteOne({_id: id});
-  }
+async function getAllUsers() {
+  const users = await User.find({});
+  return users;
 }
 
-module.exports = UserResource;
+async function getUserById(id) {
+  const user = await User.findOne({_id: id});
+  return user;
+}
+
+async function getUserByUsername(username) {
+  const user = await User.findOne({username});
+  return user;
+}
+
+async function createUser(params) {
+  const user = await User.create({
+    ...params
+  });
+  return user;
+}
+
+async function updateUserById(id, params) {
+  await User.updateOne(
+    {
+      _id: id
+    },
+    {
+      ...params
+    }
+  );
+}
+
+async function deleteUserById(id) {
+  await User.deleteOne({_id: id});
+}
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  getUserByUsername,
+  createUser,
+  updateUserById,
+  deleteUserById
+};
