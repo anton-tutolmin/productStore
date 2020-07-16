9035d7eb20f4a2e134582fefb52717d9a11d30b5
Anton Tutolmin <crysisage@gmail.com>
2020-07-16 10:59:12 +0600

    fix user router_1

--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -4,12 +4,24 @@ const UserController = require('../controllers/userController');
 const router = new KoaRouter();
 
 router
-  .get('/users', UserController.getAllUsers)
+  .get('/users', async (ctx, next) => {
+    const users = await UserController.getAllUsers();
+    ctx.response.body = {users};
+  })
 
-  .get('/users/:id', UserController.getUserById)
+  .get('/users/:id', async (ctx, next) => {
+    const user = await UserController.getUserById(ctx.params.id);
+    ctx.response.body = {user};
+  })
 
-  .put('/users/:id', UserController.updateUserById)
+  .put('/users/:id', async (ctx, next) => {
+    await UserController.updateUserById(ctx.params.id);
+    ctx.response.body = {message: 'User updated'}
+  })
 
-  .delete('/users/:id', UserController.deleteUserById)
+  .delete('/users/:id', async (ctx, next) => {
+    await UserController.deleteUserById(ctx.params.id);
+    ctx.response.body = {message: 'User deleted'};
+  })
 
 module.exports = router;
