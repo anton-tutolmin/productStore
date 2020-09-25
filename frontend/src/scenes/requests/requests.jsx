1bd7a5d39aa5d9cc0d4ecc8277a10a4568368fe7
Anton Tutolmin <crysisage@gmail.com>
2020-09-25 14:35:16 +0600

    requests loader

--- frontend/src/scenes/requests/requests.jsx
+++ frontend/src/scenes/requests/requests.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { RequestItem } from '../../components/request/requestItem.jsx';
 import { doUpdateOrder } from '../../store/actions/async/order';
+import { Loader } from '../../components/loader/loader.jsx';
 import './requests.sass';
 
 const Requests = (props) => {
-  const { requests, takeRequest } = props;
+  const { requests, loading, takeRequest } = props;
 
   const onTakeRequest = (orderId) => {
     takeRequest(orderId, 'delivering');
@@ -13,22 +14,27 @@ const Requests = (props) => {
 
   return (
     <div className="requests">
-      {requests.map((r) => (
-        <div className="request__container" key={r.id}>
-          <RequestItem
-            product={r.product}
-            client={r.client}
-            orderId={r.id}
-            take={onTakeRequest}
-          />
-        </div>
-      ))}
+      {loading ? (
+        <Loader />
+      ) : (
+        requests.map((r) => (
+          <div className="request__container" key={r.id}>
+            <RequestItem
+              product={r.product}
+              client={r.client}
+              orderId={r.id}
+              take={onTakeRequest}
+            />
+          </div>
+        ))
+      )}
     </div>
   );
 };
 
 const mapStateToProps = (state) => ({
   requests: state.requests.requests,
+  loading: state.requests.loading,
 });
 
 const mapDispatchToProps = (dispatch) => ({
