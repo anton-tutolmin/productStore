67b48f1bf2b7f05c34833dd9339c2770e970c72d
Anton Tutolmin <crysisage@gmail.com>
2020-10-17 18:24:32 +0600

    cacher

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ const KoaBody = require('koa-body');
 const send = require('koa-send');
 const serve = require('koa-static');
 const logger = require('./middleware/logger');
+const cacher = require('./cacher/cacher');
 
 const passport = require('koa-passport');
 
@@ -29,6 +30,7 @@ app.use(passport.initialize());
 
 app.use(async (ctx, next) => {
   try {
+    ctx.cacher = cacher;
     await next();
   } catch (e) {
     ctx.response.status = 200;
